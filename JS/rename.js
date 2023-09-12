1125115e375896efdd4cf8bbee7a97e96b834091
Scpshaku <84615903+Scpshaku@users.noreply.github.com>
2023-09-12 20:53:48 +0800

    Update rename.js

--- JS/rename.js
+++ JS/rename.js
@@ -70,6 +70,19 @@ var autofill = parseInt($arguments.autofill) || false;
 // 获取机场名
 const airport = ($arguments.airport == undefined) ? '' : decodeURI($arguments.airport);
 
+//删除非必要的1
+function stripOnes(proxies) {
+  Object.keys(countries).forEach((item,index,array)=>{
+    if (countries[item][1] === 1) {
+      proxies.map((res) => {
+        if (res.name.indexOf(countries[item][0]) !== -1) {
+          res.name = res.name.replace("1", '').replace('0', '');
+        };
+      });
+    };
+  });
+  return proxies
+};
 
 // 简繁转换
 function charPYStr() {
